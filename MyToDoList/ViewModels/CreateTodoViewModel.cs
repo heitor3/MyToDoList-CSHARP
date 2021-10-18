851fd22ab8f72e16291d150dd97e5986bb0758f4
Heitor Pires <heitorfernandes_3@hotmail.com>
2021-10-18 18:18:45 -0300

    finish miniAPI TodoList

---
+++ MyToDoList/ViewModels/CreateTodoViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MyToDoList.ViewModels
+{
+    public class CreateTodoViewModel
+    {
+        [Required]
+        public string Title { get; set; }
+
+        [Required]
+        public string Description { get; set; }
+    }
+}
